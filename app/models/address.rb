c935e32175781c37f3343ff44972ab99bc793f44
Marli Baumann <marli@baumannpaper.com>
2009-09-24 01:52:28 -0400

    Added lat long geocoding to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,20 @@
 class Address < ActiveRecord::Base
 
+  require 'geokit'
+
+  before_create :find_lat_long
+
   def latlong
     "#{lat}, #{long}"
   end
 
   def full_address
-    [street_line_1, street_line_2, city, state, zip].join(', ')
+    [street_line_1, city, state, zip].join(', ')
   end
 
+  def find_lat_long
+    address = Geokit::Geocoders::GoogleGeocoder.geocode full_address
+    self.lat = address.lat
+    self.long = address.lng
+  end
 end
