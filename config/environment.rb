564afa62ac9d026adedf470b5190f8b682aed0a8
Marli Baumann <marli@baumannpaper.com>
2009-11-03 19:28:51 -0500

    added prefix to the url

--- config/environment.rb
+++ config/environment.rb
@@ -39,3 +39,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+ActionController::AbstractRequest.relative_url_root = "/master_commissioner" if RAILS_ENV == 'production'
