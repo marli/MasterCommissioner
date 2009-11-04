c101f70eb8f3665ba98c7451ca58365c966e7687
Marli Baumann <marli@baumannpaper.com>
2009-11-03 19:50:10 -0500

    added nokogiri gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file
+ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-ENV['RAILS_ENV'] ||= 'production'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
