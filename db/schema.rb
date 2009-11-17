d4f2536718d0907b6aee05e0b83b645a38f614ff
Marli Baumann <marli@baumannpaper.com>
2009-11-16 22:03:12 -0500

    added user observer and fixed routing error

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091012221016) do
+ActiveRecord::Schema.define(:version => 20091117025011) do
 
   create_table "addresses", :force => true do |t|
     t.string   "street_line_1"
@@ -31,6 +31,20 @@ ActiveRecord::Schema.define(:version => 20091012221016) do
     t.string   "plaintiff"
     t.string   "defendant"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
   create_table "websites", :force => true do |t|
     t.string   "webaddress"
