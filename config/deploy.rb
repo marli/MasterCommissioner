30db8a311c654224549b4223541f3c472dba980c
Marli Baumann <marli@baumannpaper.com>
2010-06-06 19:01:11 -0400

    updated deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,3 +17,18 @@ set :scm, :git
 role :app, location
 role :web, location
 role :db,  location, :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+     
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+       
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
