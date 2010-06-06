f16de1118fe60caf2001edd689f63ccfd2a9b513
Marli Baumann <marli@baumannpaper.com>
2010-06-06 18:19:40 -0400

    updated deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,14 @@
 set :application, "MasterCommissioner"
+set :user, "marlibau"
+set :branch, "master"
 set :repository,  "git@github.com:marli/MasterCommissioner.git"
 set :domain, 'marlibau@marlibaumann.com'
+set :location, "marlibaumann.com"
+set :deploy_to, "/home/marlibau/rails/#{application}"
+set :deploy_via, :remote_cache
 
 set :scm, :git
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
-
+role :app, location
+role :web, location
+role :db,  location, :primary => true
